b80f01019d216bb55ceab3994050a4ad862f1111
Joe Fleming <joe.fleming@gmail.com>
2017-02-14 19:26:35 -0700

    add login page

---
+++ src/pages/Login.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <div class="notification">
+        Login Here
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+  export default {
+    name: 'LoginPage',
+  }
+</script>
