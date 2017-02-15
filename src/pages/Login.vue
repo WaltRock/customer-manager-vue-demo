0996117565914fe93af97a7d52e16ce339eb0547
Joe Fleming <joe.fleming@gmail.com>
2017-02-14 19:44:30 -0700

    add authentication requirement
    and a mock auth helper

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,11 +1,21 @@
 <template>
-  <section class="section">
-    <div class="container">
-      <div class="notification">
-        Login Here
+  <div id="login" class="container">
+    <nav class="nav has-shadow">
+      <div class="nav-left">
+        <div class="nav-item is-brand">
+          <router-link class="nav-item is-brand" to="/">Customer Management</router-link>
+        </div>
       </div>
-    </div>
-  </section>
+    </nav>
+
+    <section class="section">
+      <div class="container">
+        <div class="notification">
+          Login Here
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
--- src/router.js
+++ src/router.js
@@ -3,19 +3,26 @@ import VueRouter from 'vue-router';
 
 import App from './App.vue';
 
+import Login from './pages/Login.vue';
 import Customers from './pages/Customers.vue';
 import Orders from './pages/Orders.vue';
 import About from './pages/About.vue';
+
+import { getUser } from './lib/authentication';
 
 Vue.use(VueRouter);
 
 const routes = [{
   path: '/login',
-
+  name: 'login',
+  component: Login,
 }, {
   path: '/',
   name: 'app',
   component: App,
+  meta: {
+    requiresAuthentication: true,
+  },
   children: [
     {
       path: 'customers',
@@ -39,6 +46,23 @@ const routes = [{
 const router = new VueRouter({
   mode: 'hash',
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  // check if any of the matched routes require authentication
+  if (to.matched.some(record => record.meta.requiresAuthentication)) {
+    return getUser().then((user) => {
+      // if user is authenticated, continue
+      if (user) return next();
+
+      // otherwise, redirect to login, preserving the requested route
+      const { name, fullPath } = to;
+      const query = (name !== undefined) ? { redirect: name } : { prev: fullPath };
+      return next({ name: 'login', query });
+    });
+  }
+
+  return next();
 });
 
 export default router;
